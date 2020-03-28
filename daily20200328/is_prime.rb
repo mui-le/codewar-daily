312e99b869e46e49d2733ae57b9e01ea07ff5f5a
legolas-le <ngonthapvinhhang@gmail.com>
2020-03-28 22:52:23 +0700

    [daily] fix the first range

--- daily20200328/is_prime.rb
+++ daily20200328/is_prime.rb
@@ -1,3 +1,3 @@
 def isPrime(num)
-  num < 2 ? false : (num <= 3 || (3..(num**0.5 + 1).round).select { |n| num % n == 0 }.none? )
+  num < 2 ? false : (num <= 3 || (2..(num**0.5 + 1).round).select { |n| num % n == 0 }.none? )
 end
